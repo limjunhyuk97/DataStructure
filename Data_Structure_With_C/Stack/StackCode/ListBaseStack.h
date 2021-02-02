9fdb2edd6c055066ca06f129f6cc6bb8bb36626d
Jun Hyuk Lim <59442344+limjunhyuk97@users.noreply.github.com>
2021-02-02 21:06:56 +0900

    Create ListBaseStack.h

---
+++ Data_Structure_With_C/Stack/StackCode/ListBaseStack.h
@@ -0,0 +1,28 @@
+#ifndef __LIST_BASE_STACK_H__
+#define __LIST_BASE_STACK_H__
+
+#define TRUE 1
+#define FALSE 0
+#define STACK_LEN 100
+
+typedef int SData;
+
+typedef struct _node {
+	SData data;
+	struct _node* next;
+}Node;
+
+typedef struct _listStack {
+	Node* head;
+} ListStack;
+
+typedef ListStack Stack;
+
+void StackInit(Stack* pstack);
+int SIsEmpty(Stack* pstack);
+void SPush(Stack* pstack, SData data);
+SData SPop(Stack* pstack);
+SData SPeek(Stack* pstack);
+
+
+#endif
